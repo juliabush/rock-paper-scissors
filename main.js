7f5ce37b48350301d8d7fc33970d82761d01c34b
juliabush <j.elizabethbush@gmail.com>
2025-02-19 02:17:57 +0100

    Created the mobile rules page

---
+++ main.js
@@ -0,0 +1,22 @@
+const rulesSection = document.getElementById("rules-section");
+const ruleButton = document.getElementById("rule-button");
+const gameHeader = document.getElementById("header");
+const gameFooter = document.getElementById("footer");
+const triangleGameButtons = document.getElementById("tri");
+const gameBody = document.getElementById("body");
+const rulesCloseButton = document.getElementById("rules-close-button");
+
+ruleButton.addEventListener("click", () => {
+    gameHeader.classList.add("hidden");
+    gameFooter.classList.add("hidden");
+    triangleGameButtons.classList.add("hidden");
+    document.getElementById('rules-section').style.display = 'grid';
+    gameBody.classList.add('white');
+})
+rulesCloseButton.addEventListener("click", () => {
+    gameHeader.classList.remove("hidden");
+    gameFooter.classList.remove("hidden");
+    triangleGameButtons.classList.remove("hidden");
+    document.getElementById('rules-section').style.display = 'none';
+    gameBody.classList.remove('white');
+})
